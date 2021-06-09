86f41e66fec497effff2aa178682feb943f83570
Fredrik <fredrsan@gmail.com>
2021-06-09 14:13:31 +0200

    Sign up page should be working now

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -3,31 +3,53 @@ import { END_POINTS, fetchApiAuth } from "../api";
 
 function SignUpPage() {
   const [errors, setErrors] = useState([]);
+  const [ignorePasswordValidation, setIgnorePasswordValidation] =
+    useState(false);
+
   const usernameRef = useRef(null);
   const passwordRef = useRef(null);
   const password2Ref = useRef(null);
+  const checkboxRef = useRef(null);
+
+  const handleToggle = (e) => {
+    if (checkboxRef.current === e.target) {
+      setIgnorePasswordValidation(!ignorePasswordValidation);
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setErrors([]);
-    console.log(passwordRef.current.value);
-    if (
-      passwordRef.current.value === null ||
-      passwordRef.current.value === ""
-    ) {
+
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+    const passwordTwo = password2Ref.current.value;
+
+    console.log(ignorePasswordValidation);
+
+    if (password === null || password === "") {
       setErrors([["Password", "Password cannot be empty"]]);
       return;
     }
-    if (passwordRef.current === password2Ref.current) {
-      //const response = await fetchApiAuth(
-      //{ username: usernameRef, password: passwordRef },
-      //END_POINTS.POST_USER_CREATE
-      //);
-      //console.log(response);
-      console.log("success");
+    if (password === passwordTwo) {
+      const errors = [];
+      const data = {
+        username: username,
+        password: password,
+        ignore_password_validation: ignorePasswordValidation,
+      };
+      const response = await fetchApiAuth(data, END_POINTS.POST_USER_CREATE);
+      console.log(response);
+      if (response.password) {
+        response.password.forEach((error) => errors.push(["Password", error]));
+      }
+      if (response.username) {
+        response.username.forEach((error) => errors.push(["Username", error]));
+      }
+      setErrors(errors);
       return;
     }
-    setErrors([["Password", "Confirm password and Password must match"]]);
+    setErrors([["Password", "Confirm Password and Password must match"]]);
   };
 
   return (
@@ -40,6 +62,12 @@ function SignUpPage() {
         <input ref={passwordRef} type="password" />
         <label htmlFor="password2">Confirm Password:</label>
         <input ref={password2Ref} type="password" />
+        <div className="form-checkbox-wrapper">
+          <label htmlFor="ignorePasswordValidation">
+            Ignore password validation:
+          </label>
+          <input ref={checkboxRef} type="checkbox" onClick={handleToggle} />
+        </div>
         {errors && (
           <ul className="login-errors-wrapper">
             {errors.map((error, index) => {
